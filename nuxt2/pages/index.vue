e5f21e9f81150c11d0411a1baaee30a1817000ae
Jeroen Claessens <jclaessens97@hotmail.com>
2022-04-19 15:56:00 +0200

    added nuxt2 webpack project

---
+++ nuxt2/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Counter />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Counter } from "lib/dist/lib.es";
+
+export default Vue.extend({
+  components: {
+    Counter,
+  },
+});
+</script>
