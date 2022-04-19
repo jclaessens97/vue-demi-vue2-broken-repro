c23ad1523356207f4140ad765a12537383ab3200
Jeroen Claessens <jclaessens97@hotmail.com>
2022-04-19 15:46:59 +0200

    added vue2-vite project

---
+++ vue2-vite/src/App.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { Counter } from 'lib';
+
+export default {
+  components: {
+    Counter,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Counter />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
